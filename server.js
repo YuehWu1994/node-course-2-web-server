92f7c70eee981d49880c62a25d097e57f6d6994c
Apple <rex9999772001@gmail.com>
2018-10-27 15:27:43 -0700

    setup start script and Heroku port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+// 1st condition is for heroku
+const port = process.env.PORT || 3000;
 var app = express();
 
 // give path access to html
@@ -59,6 +60,6 @@ app.get('/bad', (req, res) => {
   });
 })
 
-app.listen(3000, () => {
-  console.log('server is on');
+app.listen(port, () => {
+  console.log(`server is on port ${port}`);
 });
